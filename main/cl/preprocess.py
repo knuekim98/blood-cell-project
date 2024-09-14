569f7402c8eb6d2c9f98b587348b2d18142c5375
Pi <knuekim98@naver.com>
2024-09-14 22:36:29 +0900

    preprocessing

---
+++ main/cl/preprocess.py
@@ -0,0 +1,41 @@
+import h5py
+import numpy as np
+import pandas as pd
+import os
+from PIL import Image
+
+
+labels = pd.read_csv("./dataset/dataset-master/labels.csv")
+X = []
+y = []
+
+for f in os.listdir("./dataset/dataset-master/JPEGImages/"):
+    img = Image.open("./dataset/dataset-master/JPEGImages/"+f)
+    img = np.array(img)
+
+    assert img.shape == (480, 640, 3)
+    
+    # one-hot encoding
+    img_number = int(f[11:16])
+    cells = labels.loc[labels["Image"]==img_number, "Category"].item()
+    if not isinstance(cells, str): continue
+
+    #print(cells, img_number, type(cells))
+
+    cells_one_hot = np.array([
+        int('NEUTROPHIL' in cells),
+        int('EOSINOPHIL' in cells),
+        int('LYMPHOCYTE' in cells),
+        int('MONOCYTE' in cells),
+        int('BASOPHIL' in cells)
+    ])
+
+    X.append(img)
+    y.append(cells_one_hot)
+
+
+print(len(X), len(y))
+
+with h5py.File('./main/cl/dataset.h5', 'w') as file:
+    file.create_dataset("x", data=X)
+    file.create_dataset("y", data=y)
